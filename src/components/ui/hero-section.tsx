14f749b899f20db8fe920248e95b028163fa1c13
rtrivaldo <rtrivaldo05@gmail.com>
2024-08-11 13:09:30 +0700

    add links hover on hero section

--- src/components/ui/hero-section.tsx
+++ src/components/ui/hero-section.tsx
@@ -1,9 +1,12 @@
-import { BackgroundBeams } from "@/components/ui/background-beams";
+import Link from "next/link";
+
 import Aos from "aos";
 import "aos/dist/aos.css";
-import Link from "next/link";
+
+import { BackgroundBeams } from "@/components/ui/background-beams";
 import { useEffect } from "react";
 import { RiArrowDownSLine } from "react-icons/ri";
+import { LinkPreview } from "./link-preview";
 
 export default function HeroSection() {
     useEffect(() => {
@@ -13,14 +16,25 @@ export default function HeroSection() {
     });
     return (
         <div className="h-screen flex flex-col justify-center max-w-screen-xl px-6 md:px-10 mx-auto">
+            <BackgroundBeams />
             <div className="">
-                <div className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl leading-tight font-bold animate-dimmed" id="heroSection">
+                <div className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl leading-tight font-bold animate-dimmed">
                     <h1>
-                        <span className="opacity-30 animate-dimmed [animation-delay:1400ms]">👋 Hi, I&apos;m</span> <span className="text-cyan-400 opacity-30 animate-dimmed delay-500">Rivaldo Tandoko. </span>
+                        <span className="opacity-30 animate-dimmed [animation-delay:1400ms]">
+                            <span className="animate-hand-wave inline-block origin-[70%_70%] [animation-delay:2700ms]">👋</span> Hi, I&apos;m
+                        </span>{" "}
+                        <LinkPreview url="https://github.com/rtrivaldo">
+                            <span className="text-cyan-400 opacity-30 animate-dimmed delay-500">Rivaldo Tandoko.</span>
+                        </LinkPreview>{" "}
                         <br className="hidden sm:block" />
-                        <span className="opacity-30 animate-dimmed [animation-delay:1700ms]">A</span> <span className="text-cyan-400 opacity-30 animate-dimmed [animation-delay:800ms]">frontend developer,</span> <span className="opacity-30 animate-dimmed [animation-delay:2000ms]">who craft </span>
-                        <br className="hidden sm:block" />
-                        <span className="text-cyan-400 opacity-30 animate-dimmed [animation-delay:1100ms]">digital experiences.</span>
+                        <span className="opacity-30 animate-dimmed [animation-delay:1700ms]">A</span>{" "}
+                        <LinkPreview url="https://github.com/rtrivaldo">
+                            <span className="text-cyan-400 opacity-30 animate-dimmed [animation-delay:800ms]">frontend developer,</span>
+                        </LinkPreview>{" "}
+                        <span className="opacity-30 animate-dimmed [animation-delay:2000ms]">who craft</span> <br className="hidden sm:block" />
+                        <LinkPreview url="https://github.com/rtrivaldo">
+                            <span className="text-cyan-400 opacity-30 animate-dimmed [animation-delay:1100ms]">digital experiences.</span>
+                        </LinkPreview>
                     </h1>
                 </div>
                 <p className="hidden md:block lg:mt-4 font-semibold text-lg underline underline-offset-8 decoration-cyan-400 decoration-2 opacity-0 animate-fade-down [animation-delay:2500ms]">Fullstack Capable</p>
@@ -34,7 +48,6 @@ export default function HeroSection() {
                     <span className="font-medium">Scroll down</span>
                 </div>
             </Link>
-            <BackgroundBeams />
         </div>
     );
 }
